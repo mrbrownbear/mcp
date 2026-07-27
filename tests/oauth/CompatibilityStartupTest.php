a0f008437fb8f75343b9dd258c62382803a3c4a9
Daniele Parisi <daniele.parisi@ovation.it>
2026-07-27 19:18:07 +0200

    version

--- tests/oauth/CompatibilityStartupTest.php
+++ tests/oauth/CompatibilityStartupTest.php
@@ -136,7 +136,7 @@ final class CompatibilityStartupTest extends TestCase
         );
         self::assertSame(
             [
-                'plugin_version' => '1.10.1',
+                'plugin_version' => '1.11.0',
                 'rest_api_version' => 1,
                 'wordpress_version' => '6.9.2',
                 'minimum_wordpress_version' => '6.9',
